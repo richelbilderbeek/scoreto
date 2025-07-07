209b2ff464fef3abf2d8e587a8b704d16ecd98d7
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-07 13:28:21 +0200

    Can get all courses combined

---
+++ R/get_courses.R
@@ -0,0 +1,11 @@
+#' Get all the courses
+#' @return a table with all courses.
+#' @export
+get_courses <- function() {
+  tibbles <- list()
+  tibbles[[1]] <- get_adh_courses()
+  tibbles[[2]] <- get_naiss_courses()
+  tibbles[[3]] <- get_sll_courses()
+  t <- dplyr::bind_rows(tibbles)
+  t |> tidyr::drop_na()
+}
---
+++ tests/testthat/test-get_courses.R
@@ -0,0 +1,7 @@
+test_that("use", {
+  t <- get_courses()
+  expect_true(tibble::is_tibble(t))
+  expect_equal(5, length(names(t)))
+  expect_equal(c("date_from", "date_to", "name", "url", "source"), names(t))
+  expect_equal(0, is.na(t$date_from))
+})
