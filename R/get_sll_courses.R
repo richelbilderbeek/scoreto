b9242168b72098260a629bc236f4ac6a17a68ec1
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-07 09:13:43 +0200

    Can collect SLL courses

--- R/get_sll_courses.R
+++ R/get_sll_courses.R
@@ -1,7 +1,23 @@
 #' Get the SciLifeLab courses
-#' @return a table with all SciLifeLab courses
+#' @return
+#'   a table with all SciLifeLab courses.
+#'
+#'   This table is a \link[tibble]{tibble} with column names `name`
+#'   and `url`.
 #' @export
 get_sll_courses <- function() {
-  html <- rvest::read_html("https://training.scilifelab.se/events")
-  html
+  lines <- readr::read_lines("https://training.scilifelab.se/events")
+
+    course_names_with_nas <- stringr::str_match(lines, "name\": \"(.*)\",")[, 2]
+  course_names <- course_names_with_nas[!is.na(course_names_with_nas)]
+
+  urls_with_nas <- stringr::str_match(lines, "url\": \"(.*)\",")[, 2]
+  urls <- urls_with_nas[!is.na(urls_with_nas)]
+
+  testthat::expect_equal(length(course_names), length(urls))
+
+  tibble::tibble(
+    name = course_names,
+    url = urls
+  )
 }
--- tests/testthat/test-get_sll_courses.R
+++ tests/testthat/test-get_sll_courses.R
@@ -1,3 +1,8 @@
 test_that("use", {
   expect_silent(get_sll_courses())
+
+  t <- get_sll_courses()
+  expect_true(tibble::is_tibble(t))
+  expect_equal(2, length(names(t)))
+  expect_equal(c("name", "url"), names(t))
 })
