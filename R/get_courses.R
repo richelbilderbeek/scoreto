de1ebaab95860ede9ed7e69afcddb559fba15a01
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-08 14:11:46 +0200

    Return only relevant courses (i.e. those that take place in the future)

--- R/get_courses.R
+++ R/get_courses.R
@@ -1,5 +1,6 @@
-#' Get all the courses
-#' @return a table with all courses.
+#' Get all the future courses
+#'
+#' @return a table with all future courses, sorted alphabetically by date
 #' @export
 get_courses <- function() {
   tibbles <- list()
@@ -15,6 +16,10 @@ get_courses <- function() {
   # TODO: Add NSC
   # TODO: Add LUNARC
 
-  t <- dplyr::bind_rows(tibbles)
-  t |> tidyr::drop_na()
+  t_with_nas <- dplyr::bind_rows(tibbles)
+  t_any_date <- t_with_nas |> tidyr::drop_na()
+  t <- t_any_date  |>
+    dplyr::filter(date_to >= lubridate::today()) |>
+    dplyr::arrange(date_from)
+  t
 }
