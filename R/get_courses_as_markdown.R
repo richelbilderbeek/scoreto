72e0eed2b8bc3a094b94127a20943de71b3a3bf6
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-08 09:41:49 +0200

    Prettier Markdown table

--- R/get_courses_as_markdown.R
+++ R/get_courses_as_markdown.R
@@ -4,5 +4,20 @@
 #' @export
 get_courses_as_markdown <- function() {
   t <- get_courses()
-  knitr::kable(t)
+
+  # Convert URLs to Markdown strings
+  t$url <- paste0("[Course site](", t$url, ")")
+
+  # Convert URLs to Markdown strings
+  t$source <- paste0("[Provider site](", t$source, ")")
+
+  text <- knitr::kable(t)
+  #message(text)
+  #max(nchar(t$name))
+  testthat::expect_match(text[1], "|date_from[:blank:]*|date_to[:blank:]*|name[:blank:]*|url[:blank:]*|source[:blank:]*|")
+  text[1] <- "|**From**|**To**|**Course name**|**Course site**|**Provider site**|"
+  testthat::expect_match(text[2], "|:-+|:-+|:-+|:-+|:-+|")
+  #           | 2025-01-01| 2025-01-01|Long course name                                    |Course site |Provider site
+  text[2] <- "|:----------|:----------|:---------------------------------------------------|:-----------|:------------|"
+  text
 }
