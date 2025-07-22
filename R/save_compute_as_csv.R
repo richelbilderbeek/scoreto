09d6c4a436928676ca5b5bf4ef1e36d194a2fb9b
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-22 12:07:01 +0200

    Remove 'save_compute_as_csv'

--- R/save_compute_as_csv.R
+++
@@ -1,9 +0,0 @@
-#' Save all the courses, as a CSV text
-#'
-#' @param csv_filename path to the file to be created
-#' @return Nothing
-#' @export
-save_compute_as_csv <- function(csv_filename = "compute.csv") {
-  t <- get_compute()
-  readr::write_csv(t, csv_filename)
-}
