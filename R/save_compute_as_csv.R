b850036a6166341a00141ae6fc708f9853dea58b
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-22 10:22:33 +0200

    Add 'save_compute_as_csv'

--- R/save_compute_as_csv.R
+++ R/save_compute_as_csv.R
@@ -3,7 +3,7 @@
 #' @param csv_filename path to the file to be created
 #' @return Nothing
 #' @export
-save_courses_as_csv <- function(csv_filename = "courses.csv") {
-  t <- get_courses()
+save_compute_as_csv <- function(csv_filename = "compute.csv") {
+  t <- get_compute()
   readr::write_csv(t, csv_filename)
 }
--- tests/testthat/test-save_compute_as_csv.R
+++ tests/testthat/test-save_compute_as_csv.R
@@ -1,7 +1,7 @@
 test_that("use", {
   filename <- tempfile()
   expect_false(file.exists(filename))
-  save_courses_as_csv(filename)
+  save_compute_as_csv(filename)
   expect_true(file.exists(filename))
   t <- readr::read_csv(filename)
   expect_true(tibble::is_tibble(t))
