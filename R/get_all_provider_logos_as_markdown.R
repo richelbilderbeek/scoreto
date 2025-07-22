bdd5330772da25cccbf92e5b45682e0b19814fdb
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-22 11:21:34 +0200

    Use 'cat'

--- R/get_all_provider_logos_as_markdown.R
+++ R/get_all_provider_logos_as_markdown.R
@@ -7,5 +7,5 @@ get_all_provider_logos_as_markdown <- function() {
   providers <- get_all_providers()
   logo_paths <- get_logo_paths(providers)
   testthat::expect_equal(length(providers), length(logo_paths))
-  paste0("![", providers, "](", logo_paths, ")")
+  cat(paste0("![", providers, "](", logo_paths, ")"))
 }
