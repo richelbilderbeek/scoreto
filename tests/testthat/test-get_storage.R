2002c4de2691d9e2669df77b5334a2952fa4a2ac
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-23 13:46:36 +0200

    Fix data

--- tests/testthat/test-get_storage.R
+++ tests/testthat/test-get_storage.R
@@ -1,3 +1,11 @@
 test_that("use", {
   expect_silent(get_storage())
 })
+
+test_that("file storage.csv is OK", {
+  filename <- get_scoreto_path("storage.csv")
+  text <- readr::read_lines(filename)
+  n_commas <- stringr::str_count(text, ",")
+  expect_true(all(n_commas == n_commas[1]))
+})
+
