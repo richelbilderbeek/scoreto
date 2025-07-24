5be6a0871cfc4b719807990521c5bf5c7780d083
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-24 11:21:12 +0200

    Column values start with upper case

--- tests/testthat/test-get_storage.R
+++ tests/testthat/test-get_storage.R
@@ -6,3 +6,12 @@ test_that("file storage.csv is OK", {
   expect_silent(get_storage(check_file = TRUE))
 })
 
+test_that("Some column values start with uppercase", {
+  t <- get_storage()
+
+  expect_true(all(stringr::str_detect(t$data_sensitivity, "^[:upper:]")))
+  expect_true(all(stringr::str_detect(t$data_activity, "^[:upper:]")))
+  expect_true(all(stringr::str_detect(t$user_costs, "^\\[?[:upper:]")))
+  expect_true(all(stringr::str_detect(t$accessible_for, "^[:upper:]")))
+})
+
