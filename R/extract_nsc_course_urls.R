1540b7931a16cf8fe4ae81cb36fa85739cbeb766
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-07-25 12:59:22 +0200

    Fix NSC, progress https://github.com/NBISweden/SCoRe_user_doc/issues/16

--- R/extract_nsc_course_urls.R
+++ R/extract_nsc_course_urls.R
@@ -1,15 +1,15 @@
 #' Extract the URL of a course from an NSC courses text
 #' @param nsc_courses_text subset of raw HTML from the NSC website.
-#' @param nsc_training_url The URL for the NSC courses
+#' @param nsc_home_url The URL for the NSC home
 #' @return a collection of strings and NAs
 #' @export
 extract_nsc_course_urls <- function(
   nsc_courses_text,
-  nsc_training_url = "https://www.nsc.liu.se/support/Events/"
+  nsc_home_url = "https://www.nsc.liu.se"
 ) {
   rel_urls <- stringr::str_match(
     nsc_courses_text,
     "<a href=\"(.*)\">"
   )[, 2]
-  paste0(nsc_training_url, rel_urls)
+  paste0(nsc_home_url, rel_urls)
 }
--- tests/testthat/test-extract_nsc_course_urls.R
+++ tests/testthat/test-extract_nsc_course_urls.R
@@ -2,4 +2,9 @@ test_that("use", {
   nsc_courses_text <- get_nsc_test_courses_text()
   course_urls <- extract_nsc_course_urls(nsc_courses_text)
   expect_equal(length(nsc_courses_text), length(course_urls))
+
+  expect_equal(
+    0,
+    sum(stringr::str_detect(course_urls, "support/Events//support"))
+  )
 })
