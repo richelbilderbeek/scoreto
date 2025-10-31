1663e2313e457f05ff2855c878db69d7ef113454
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-10-31 07:35:43 +0100

    Can work with Code Refinery having no courses

--- R/get_code_refinery_courses.R
+++ R/get_code_refinery_courses.R
@@ -9,14 +9,21 @@ get_code_refinery_courses <- function(html_text = scoreto::get_code_refinery_htm
     all_lines,
     "<h2 id=\"upcoming-coderefinery-workshops\">Upcoming CodeRefinery workshops</h2>" # nolint
   ) + 1
+  testthat::expect_equal(1, length(from_index))
 
   to_index <- stringr::str_which(
     all_lines,
     "<h2 id=\"recent-workshops-and-events\">Recent workshops and events</h2>"
   ) - 1
+  testthat::expect_equal(1, length(to_index))
 
   section_lines <- all_lines[from_index:to_index]
   lines <- stringr::str_subset(section_lines, "<li>.*</li>")
+
+  if (length(lines) == 0) {
+    empty_tibble <- scoreto::create_test_courses_table()[c(), ]
+    return(empty_tibble)
+  }
 
   from_dates <- scoreto::extract_code_refinery_from_dates(lines)
   to_dates <- scoreto::extract_code_refinery_to_dates(lines)
