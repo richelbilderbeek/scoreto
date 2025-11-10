b8e2e0fdba2735b06bcbd55808529faeef582a22
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-11-10 10:34:36 +0100

    Start parsing NAISS website

--- tests/testthat/tmp/test-get_naiss_courses.R
+++
@@ -1,9 +0,0 @@
-test_that("use on today's data", {
-  t <- get_naiss_courses()
-  expect_true(is_correctly_formatted_courses_table(t))
-})
-
-test_that("use on testing data", {
-  t <- get_naiss_courses(html_text = get_test_naiss_html())
-  expect_true(is_correctly_formatted_courses_table(t))
-})
