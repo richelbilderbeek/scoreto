bdb51de64a222eb27b0c4c93dbeaef1b411eabf6
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-11-26 15:35:14 +0100

    Simplify code

--- R/extract_english_range.R
+++ R/extract_english_range.R
@@ -5,35 +5,26 @@
 extract_english_range <- function(text) {
   testthat::expect_equal(1, length(text))
 
-  # Date:27-28 November
-  result <- stringr::str_match(
-    text,
-    "Date:[:blank:]?([:digit:]+[:blank:][:upper:][:lower:]+[:blank:][:digit:]{4})" # nolint
-  )[, 2]
-  if (!is.na(result)) {
-    return(result)
-  }
-
-  # Date:27-28 November
-  result <- stringr::str_match(
-    text,
-    "Date:[:blank:]?([:digit:]+-[:digit:]+[:blank:][:upper:][:lower:]+[:blank:][:digit:]{4})" # nolint
-  )[, 2]
-  if (!is.na(result)) {
-    return(result)
-  }
-
-  # Date:27-28 November & 1-2 December 2025
-  result <- stringr::str_match(
-    text,
+  patterns <- c(
+    # Date:27-28 November
+    "Date:[:blank:]?([:digit:]+[:blank:][:upper:][:lower:]+[:blank:][:digit:]{4})", # nolint
+    # Date:27-28 November
+    "Date:[:blank:]?([:digit:]+-[:digit:]+[:blank:][:upper:][:lower:]+[:blank:][:digit:]{4})", # nolint
+    # Date:27-28 November & 1-2 December 2025
     paste0(
       "Date:[:blank:]?",
       "([:digit:]+-[:digit:]+[:blank:][:upper:][:lower:]+",
       "[:blank:]&[:blank:][:digit:]+-[:digit:]+[:blank:][:upper:][:lower:]+",
       "[:blank:][:digit:]{4})"
     )
+  )
+  for (pattern in patterns) {
+    result <- stringr::str_match(text, pattern)[, 2]
+    if (!is.na(result)) {
+      break
+    }
+  }
 
-  )[, 2]
   testthat::expect_true(!is.na(result))
   result
 }
