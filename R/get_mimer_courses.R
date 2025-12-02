8b57f639c60cd634743390f4f4f536b690d745e0
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-12-02 15:01:26 +0100

    Add Mimer

---
+++ R/get_mimer_courses.R
@@ -0,0 +1,42 @@
+#' Get the Mimer courses
+#' @param html_text HTML text to parse, as can be obtained by
+#' \link{get_mimer_html} or \link{get_test_mimer_html}
+#' @return a table with all Mimer courses, where
+#' the table will pass the test of
+#' \link{is_correctly_formatted_courses_table}
+#' @export
+get_mimer_courses <- function(html_text = scoreto::get_mimer_html()) {
+
+  website <- rvest::read_html(paste(html_text, collapse = "\n"))
+  body <- website |> rvest::html_element("body")
+  testthat::expect_true(length(body) > 0)
+  main <- body |> rvest::html_element("main")
+  testthat::expect_true(length(main) > 0)
+
+  calendar_list <- body |> rvest::html_element(".tribe-events-calendar-list")
+  testthat::expect_true(length(calendar_list) > 0)
+
+  events <- calendar_list |> rvest::html_elements("article")
+  testthat::expect_true(length(events) > 0)
+
+  english_date_ranges <- events |> rvest::html_elements("time") |> rvest::html_text(trim = TRUE)
+
+  from_dates <- scoreto::extract_english_from_dates(english_date_ranges = english_date_ranges)
+  to_dates <- scoreto::extract_english_to_dates(english_date_ranges = english_date_ranges)
+  testthat::expect_equal(length(from_dates), length(to_dates))
+
+  course_names <- events |> rvest::html_node("a") |> rvest::html_attr("title")
+  testthat::expect_equal(length(from_dates), length(course_names))
+
+  course_urls <- events |> rvest::html_node("a") |> rvest::html_attr("href")
+  testthat::expect_equal(length(from_dates), length(course_urls))
+
+  tibble::tibble(
+    date_from = from_dates,
+    date_to = to_dates,
+    course_name = course_names,
+    course_url = course_urls,
+    provider_courses_url = scoreto::get_mimer_courses_url(),
+    provider_name = "Mimer"
+  )
+}
---
+++ R/get_test_mimer_html.R
@@ -0,0 +1,8 @@
+#' Get testing HTML of the Mimer courses website
+#' @seealso use \link{get_mimer_html} to get HTML text read from the
+#' Mimer courses website
+#' @return HTML text
+#' @export
+get_test_mimer_html <- function() {
+  readr::read_lines(scoreto::get_scoreto_path("mimer.html"))
+}
