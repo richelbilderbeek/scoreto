43cac597ea4bdbe07f0dd316bb77a680492dc76d
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-12-19 09:00:37 +0100

    Surround Markdown by lintignores

--- R/get_all_provider_logos_as_markdown.R
+++ R/get_all_provider_logos_as_markdown.R
@@ -7,6 +7,6 @@ get_all_provider_logos_as_markdown <- function() { # nolint long name is fine
   providers <- scoreto::get_all_providers()
   logo_paths <- scoreto::get_logo_paths(providers)
   testthat::expect_equal(length(providers), length(logo_paths))
-  markdown_text <- cat(paste0("![", providers, "](", logo_paths, ")"))
+  markdown_text <- paste0("![", providers, "](", logo_paths, ")")
   scoreto::surround_markdown_with_lintignore(markdown_text = markdown_text)
 }
--- tests/testthat/test-get_all_provider_logos_as_markdown.R
+++ tests/testthat/test-get_all_provider_logos_as_markdown.R
@@ -1,3 +1,3 @@
 test_that("use", {
-  expect_output(get_all_provider_logos_as_markdown())
+  expect_true(length(get_all_provider_logos_as_markdown()) > 0)
 })
