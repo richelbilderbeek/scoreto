3242312a802bcce87d8a055e2c5fd13a2908c1ed
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-12-22 12:09:47 +0100

    Fix test

--- tests/testthat/test-mark_file_as_computer_generated.R
+++ tests/testthat/test-mark_file_as_computer_generated.R
@@ -42,16 +42,15 @@ test_that("use file with markdown", {
   mark_file_as_computer_generated(filename)
   after <- readr::read_lines(filename)
 
-  HIERO
-  expect_true(before[8] == "```mermaid")
-  expect_true(before[9] == "flowchart TD")
-  expect_true(before[10] == "")
-  expect_true(before[11] == "  subgraph researcher_on_sensitive_data[Researcher, sensitive data]")
+  expect_true(before[15] == "```mermaid")
+  expect_true(before[16] == "flowchart TD")
+  expect_true(before[17] == "")
+  expect_true(before[18] == "  question_need_repo[Do you want to work actively on the data?]")
 
-  expect_true(after[8] == before[8])
-  expect_true(after[9] == before[9])
-  expect_true(after[10] == "") # Should be left untouched
-  expect_true(after[11] == before[11])
+  expect_true(after[15] == before[15])
+  expect_true(after[16] == before[16])
+  expect_true(after[17] == "") # Should be left untouched
+  expect_true(after[18] == before[18])
 
   expect_false(all(before == after))
 })
