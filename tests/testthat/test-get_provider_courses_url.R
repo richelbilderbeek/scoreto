3ef6d25b8a86fb0d6ff97fc802e338ee92c91930
richelbilderbeek <rjcbilderbeek@gmail.com>
2025-12-22 14:29:57 +0100

    Fix R style

--- tests/testthat/test-get_provider_courses_url.R
+++ tests/testthat/test-get_provider_courses_url.R
@@ -7,15 +7,48 @@ test_that("minimal use", {
 })
 
 test_that("specific use", {
-  expect_equal(get_provider_courses_url("ENCCS"), "https://enccs.se/events")
-  expect_equal(get_provider_courses_url("SLUBI"), "https://www.slubi.se/courses.html")
-  expect_equal(get_provider_courses_url("SciLifeLab"), "https://training.scilifelab.se/events")
-  expect_equal(get_provider_courses_url("PDC"), "https://www.pdc.kth.se/about/events")
-  expect_equal(get_provider_courses_url("C3SE"), "https://www.c3se.chalmers.se")
-  expect_equal(get_provider_courses_url("CSC"), "https://lumi-supercomputer.eu/events")
-  expect_equal(get_provider_courses_url("LUNARC"), "https://www.lunarc.lu.se/learning-more/training-courses")
-  expect_equal(get_provider_courses_url("Mimer"), "https://mimer-ai.eu/events-and-learning")
-  expect_equal(get_provider_courses_url("NAISS"), "https://www.naiss.se/training")
-  expect_equal(get_provider_courses_url("NSC"), "https://www.nsc.liu.se/support/Events")
-  expect_equal(get_provider_courses_url("SciLifeLab"), "https://training.scilifelab.se/events")
+  expect_equal(
+    get_provider_courses_url("ENCCS"),
+    "https://enccs.se/events"
+  )
+  expect_equal(
+    get_provider_courses_url("SLUBI"),
+    "https://www.slubi.se/courses.html"
+  )
+  expect_equal(
+    get_provider_courses_url("SciLifeLab"),
+    "https://training.scilifelab.se/events"
+  )
+  expect_equal(
+    get_provider_courses_url("PDC"),
+    "https://www.pdc.kth.se/about/events"
+  )
+  expect_equal(
+    get_provider_courses_url("C3SE"),
+    "https://www.c3se.chalmers.se"
+  )
+  expect_equal(
+    get_provider_courses_url("CSC"),
+    "https://lumi-supercomputer.eu/events"
+  )
+  expect_equal(
+    get_provider_courses_url("LUNARC"),
+    "https://www.lunarc.lu.se/learning-more/training-courses"
+  )
+  expect_equal(
+    get_provider_courses_url("Mimer"),
+    "https://mimer-ai.eu/events-and-learning"
+  )
+  expect_equal(
+    get_provider_courses_url("NAISS"),
+    "https://www.naiss.se/training"
+  )
+  expect_equal(
+    get_provider_courses_url("NSC"),
+    "https://www.nsc.liu.se/support/Events"
+  )
+  expect_equal(
+    get_provider_courses_url("SciLifeLab"),
+    "https://training.scilifelab.se/events"
+  )
 })
--- tests/testthat/test-mark_file_as_computer_generated.R
+++ tests/testthat/test-mark_file_as_computer_generated.R
@@ -14,7 +14,11 @@ test_that("use", {
 test_that("use file with markdown", {
   filename <- tempfile()
   dir.create(dirname(filename), showWarnings = FALSE)
-  file.copy(from = get_scoreto_path("compute_1.md"), to = filename, overwrite = TRUE)
+  file.copy(
+    from = get_scoreto_path("compute_1.md"),
+    to = filename,
+    overwrite = TRUE
+  )
 
   before <- readr::read_lines(filename)
   mark_file_as_computer_generated(filename)
@@ -23,7 +27,10 @@ test_that("use file with markdown", {
   expect_true(before[8] == "```mermaid")
   expect_true(before[9] == "flowchart TD")
   expect_true(before[10] == "")
-  expect_true(before[11] == "  subgraph researcher_on_sensitive_data[Researcher, sensitive data]")
+  expect_equal(
+    before[11],
+    "  subgraph researcher_on_sensitive_data[Researcher, sensitive data]"
+  )
 
   expect_true(after[8] == before[8])
   expect_true(after[9] == before[9])
@@ -36,7 +43,11 @@ test_that("use file with markdown", {
 test_that("use file with markdown", {
   filename <- tempfile()
   dir.create(dirname(filename), showWarnings = FALSE)
-  file.copy(from = get_scoreto_path("storage_1.md"), to = filename, overwrite = TRUE)
+  file.copy(
+    from = get_scoreto_path("storage_1.md"),
+    to = filename,
+    overwrite = TRUE
+  )
 
   before <- readr::read_lines(filename)
   mark_file_as_computer_generated(filename)
@@ -46,7 +57,10 @@ test_that("use file with markdown", {
   expect_true(before[15] == "```mermaid")
   expect_true(before[16] == "flowchart TD")
   expect_true(before[17] == "")
-  expect_true(before[18] == "  question_need_repo[Do you want to work actively on the data?]")
+  expect_equal(
+    before[18],
+    "  question_need_repo[Do you want to work actively on the data?]"
+  )
 
   expect_true(after[15] == before[15])
   expect_true(after[16] == before[16])
