8d2e86ff07688d9ad92b4672dae2110d01a89789
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-06-23 09:40:31 +0200

    Fix build

--- R/get_naiss_html.R
+++ R/get_naiss_html.R
@@ -1,6 +1,4 @@
 #' Get the NAISS courses website as HTML
-#' @seealso use \link{get_test_naiss_html} to get similar HTML text to be
-#' used in testing
 #' @return HTML text
 #' @export
 get_naiss_html <- function() {
