1a73cf578a4fd695284b4d68f9ad1d5ae6182ed8
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-06-23 09:36:41 +0200

    Remove obsolete tests for older NAISS training pages

--- tests/testthat/test-get_naiss_courses.R
+++ tests/testthat/test-get_naiss_courses.R
@@ -3,17 +3,6 @@ test_that("use on today's data", {
   expect_true(is_correctly_formatted_courses_table(t))
 })
 
-test_that("use on testing data", {
-  t <- get_naiss_courses(html_text = get_test_naiss_html())
-  expect_true(is_correctly_formatted_courses_table(t))
-})
-
-test_that("use on testing data, 2026-01-16", {
-  t <- get_naiss_courses(html_text = get_scoreto_path("naiss_20260116.html"))
-  expect_true(is_correctly_formatted_courses_table(t))
-})
-
-
 test_that("use on testing data, 2026-06-23", {
   # scoreto::get_provider_courses_url("NAISS")
   t <- get_naiss_courses(html_text = get_scoreto_path("naiss_20260623.html"))
