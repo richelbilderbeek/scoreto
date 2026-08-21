34a9d080c7336c1cecf9d499b71e3db88f459abf
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-08-21 12:56:39 +0200

    Use fixed HPC2N pages

--- tests/testthat/test-get_hpc2n_courses.R
+++ tests/testthat/test-get_hpc2n_courses.R
@@ -2,15 +2,16 @@ test_that("use on today's data", {
   t <- get_hpc2n_courses()
   expect_silent(check_courses_table(t))
   expect_true(all(as.logical(RCurl::url.exists(t$course_url))))
-})
-
-test_that("use on testing data", {
-  t <- get_hpc2n_courses(html_text = get_test_hpc2n_html())
-  expect_silent(check_courses_table(t))
+  get_provider_courses_url("HPC2N")
 })
 
 test_that("use, 2026-08-17", {
   t <- get_hpc2n_courses(html_text = readr::read_lines(get_scoreto_path("hpc2n_20260817.html"))) # nolint
+  expect_error(check_courses_table(t)) # Has a fault URL
+})
+
+test_that("use, 2026-08-21", {
+  t <- get_hpc2n_courses(html_text = readr::read_lines(get_scoreto_path("hpc2n_20260821.html"))) # nolint
   expect_silent(check_courses_table(t))
 })
 
