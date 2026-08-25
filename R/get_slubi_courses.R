c39f0bbece0930ce10dd0cab1838f063519c92ae
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-08-25 11:52:30 +0200

    No trailing slashes in URL

--- R/get_slubi_courses.R
+++ R/get_slubi_courses.R
@@ -37,6 +37,9 @@ get_slubi_courses <- function(html_text = scoreto::get_slubi_html()) {
   to_dates <- scoreto::extract_slubi_to_dates(english_date_ranges)
   course_urls <- scoreto::extract_slubi_course_urls(relative_urls)
 
+    # Remove trailing slashes
+  course_urls <- stringr::str_replace(course_urls, "/$", "")
+
   tibble::tibble(
     date_from = from_dates,
     date_to = to_dates,
--- tests/testthat/test-get_slubi_courses.R
+++ tests/testthat/test-get_slubi_courses.R
@@ -1,9 +1,11 @@
 test_that("use on today's data", {
   t <- get_slubi_courses()
+  expect_silent(check_courses_table(t))
   expect_true(is_correctly_formatted_courses_table(t))
 })
 
 test_that("use on testing data", {
   t <- get_slubi_courses(html_text = get_test_slubi_html())
+  expect_silent(check_courses_table(t))
   expect_true(is_correctly_formatted_courses_table(t))
 })
