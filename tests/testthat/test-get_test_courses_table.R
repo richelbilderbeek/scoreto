d1200cd38086835b7239edb260f2a6547d8fa855
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-08-25 12:15:48 +0200

    No trailing slashes in URL

--- tests/testthat/test-get_test_courses_table.R
+++ tests/testthat/test-get_test_courses_table.R
@@ -1,4 +1,6 @@
 test_that("use", {
   expect_silent(get_test_courses_table())
+  t <- get_test_courses_table()
+  expect_silent(check_courses_table(t))
   expect_true(is_correctly_formatted_courses_table(get_test_courses_table()))
 })
