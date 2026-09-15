f3115195990f94e1fcf8934a6794b39fe23a337c
richelbilderbeek <rjcbilderbeek@gmail.com>
2026-09-15 15:40:20 +0200

    Fix style

--- R/are_correctly_formatted_dates.R
+++ R/are_correctly_formatted_dates.R
@@ -1,6 +1,7 @@
 #' Are all the dates correctly formatted?
 #' @param date_strs strings for dates
-#' @return a logical vector, TRUE for each string that follows a `YYYY-MM-DD` format
+#' @return a logical vector,
+#' `TRUE` for each string that follows a `YYYY-MM-DD` format
 #' @note this does not test the value of the strings
 #' @export
 are_correctly_formatted_dates <- function(date_strs) {
--- R/check_courses_table.R
+++ R/check_courses_table.R
@@ -36,14 +36,16 @@ check_courses_table <- function(t) { # nolint indeed a long function name
     stop(
       "'t' has an NA as a course URL. \n",
       "Row indices: ", paste(which(is.na(t$course_url)), collapse = " "), "\n",
-      "Course names: ", paste(t$course_name[is.na(t$course_url)], collapse = ", ")
+      "Course names: ",
+      paste(t$course_name[is.na(t$course_url)], collapse = ", ")
     )
   }
   if (any(is.na(t$date_from))) {
     stop(
       "'t' has an NA as a from date. \n",
       "Row indices: ", paste(which(is.na(t$date_from)), collapse = " "), "\n",
-      "Course names: ", paste(t$course_name[is.na(t$date_from)], collapse = ", ")
+      "Course names: ",
+      paste(t$course_name[is.na(t$date_from)], collapse = ", ")
     )
   }
   if (any(is.na(t$date_to))) {
@@ -56,33 +58,45 @@ check_courses_table <- function(t) { # nolint indeed a long function name
   if (any(!scoreto::are_valid_urls(t$course_url))) {
     stop(
       "'t' has malformed course URLs. \n",
-      "Row indices: ", paste(which(!scoreto::are_valid_urls(t$course_url)), collapse = " "), "\n",
-      "Malformed course URLs: ", paste(t$course_url[!scoreto::are_valid_urls(t$course_url)], collapse = ", "), "\n",
-      "Course names: ", paste(t$course_name[!scoreto::are_valid_urls(t$course_url)], collapse = ", ")
+      "Row indices: ",
+      paste(which(!scoreto::are_valid_urls(t$course_url)), collapse = " "), "\n",
+      "Malformed course URLs: ",
+      paste(t$course_url[!scoreto::are_valid_urls(t$course_url)], collapse = ", "), "\n",
+      "Course names: ",
+      paste(t$course_name[!scoreto::are_valid_urls(t$course_url)], collapse = ", ")
     )
   }
   if (any(!scoreto::are_valid_urls(t$provider_courses_url))) {
     stop(
       "'t' has malformed provider URLs. \n",
-      "Row indices: ", paste(which(!scoreto::are_valid_urls(t$provider_courses_url)), collapse = " "), "\n",
-      "Malformed course URLs: ", paste(t$provider_courses_url[!scoreto::are_valid_urls(t$provider_courses_url)], collapse = ", "), "\n",
-      "Course names: ", paste(t$course_name[!scoreto::are_valid_urls(t$provider_courses_url)], collapse = ", ")
+      "Row indices: ",
+      paste(which(!scoreto::are_valid_urls(t$provider_courses_url)), collapse = " "), "\n",
+      "Malformed course URLs: ",
+      paste(t$provider_courses_url[!scoreto::are_valid_urls(t$provider_courses_url)], collapse = ", "), "\n", # nolint
+      "Course names: ",
+      paste(t$course_name[!scoreto::are_valid_urls(t$provider_courses_url)], collapse = ", ")
     )
   }
   if (any(!scoreto::are_correctly_formatted_dates(t$date_from))) {
     stop(
       "'t' has malformed from dates. \n",
-      "Row indices: ", paste(which(!!scoreto::are_correctly_formatted_dates(t$date_from)), collapse = " "), "\n",
-      "Malformed from dates: ", paste(t$date_from[!scoreto::are_correctly_formatted_dates(t$date_from)], collapse = ", "), "\n",
-      "Course names: ", paste(t$course_name[!scoreto::are_correctly_formatted_dates(t$date_from)], collapse = ", ")
+      "Row indices: ",
+      paste(which(!!scoreto::are_correctly_formatted_dates(t$date_from)), collapse = " "), "\n",
+      "Malformed from dates: ",
+      paste(t$date_from[!scoreto::are_correctly_formatted_dates(t$date_from)], collapse = ", "), "\n", # nolint
+      "Course names: ",
+      paste(t$course_name[!scoreto::are_correctly_formatted_dates(t$date_from)], collapse = ", ")
     )
   }
   if (any(!scoreto::are_correctly_formatted_dates(t$date_to))) {
     stop(
       "'t' has malformed to dates. \n",
-      "Row indices: ", paste(which(!!scoreto::are_correctly_formatted_dates(t$date_to)), collapse = " "), "\n",
-      "Malformed to dates: ", paste(t$date_to[!scoreto::are_correctly_formatted_dates(t$date_to)], collapse = ", "), "\n",
-      "Course names: ", paste(t$course_name[!scoreto::are_correctly_formatted_dates(t$date_to)], collapse = ", ")
+      "Row indices: ",
+      paste(which(!!scoreto::are_correctly_formatted_dates(t$date_to)), collapse = " "), "\n",
+      "Malformed to dates: ",
+      paste(t$date_to[!scoreto::are_correctly_formatted_dates(t$date_to)], collapse = ", "), "\n",
+      "Course names: ",
+      paste(t$course_name[!scoreto::are_correctly_formatted_dates(t$date_to)], collapse = ", ")
     )
   }
   invisible(t)
